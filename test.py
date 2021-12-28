2e4a0d57557680801ddb7c21f7bbe8988df8b566
Jonathas Moraes <jonathaslbt@gmail.com>
2021-12-28 18:19:10 -0500

    Corrigindo o Codigo

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "EPipeline Devops - Jonathas Moraes.")
+        self.assertRegex(result.data.decode(), "Pipeline DevOps - Jonathas Moraes")
 
 
 if __name__ == "__main__":
